0c107a09251350e22a11f2c1d2713812ad59654d
Rediet Ayalew <rediet.d.ayalew@gmail.com>
2017-06-12 17:05:58 -0400

    customer data base

---
+++ src/Customer.java
@@ -0,0 +1,59 @@
+
+public class Customer {
+	
+	private String fullName;
+	private String firstName;
+	private String lastName;
+	private String title;
+	private String streetAddress;
+	private String zipcode;
+	private String emailAddress;
+	
+	
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	
+	
+
+}
